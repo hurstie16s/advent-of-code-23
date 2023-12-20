024991d00f1adff9da0d41af26daef3cb13b16a8
Samuel Hurst <samuel.hurst2@btinternet.com>
2023-12-20 10:41:27 +0000

    day 2 part 1 complete

--- main/day2/day2_1.go
+++ main/day2/day2_1.go
@@ -4,9 +4,15 @@ import (
 	shared "advent-of-code-23/main/shared_code"
 	"fmt"
 	"math"
+	"sort"
 	"strconv"
 	"strings"
 )
+
+/*
+// Answer too high
+51 is incorrectly accepted
+*/
 
 const MaxRed = 12
 const MaxGreen = 13
@@ -14,13 +20,17 @@ const MaxBlue = 14
 
 func Call_2_1() {
 
-	filePath := "main/day2/day2_eg.txt"
+	//filePath := "main/day2/day2_eg.txt"
+	filePath := "main/day2/day2.txt"
+	//filePath := "main/day2/day2_testing.txt"
 
 	fileContents := shared.GetFileContents(filePath)
 
 	games := getGameMaps(fileContents)
 
 	possibleGames := getGamesPossible(games)
+	sort.Ints(possibleGames)
+	fmt.Printf("Possible Game Ids: %v\n", possibleGames)
 
 	summedGames := shared.Sum(possibleGames)
 
@@ -60,7 +70,7 @@ func getGameMaps(gameStrings []string) map[int][]int {
 	for i := 0; i < len(gameStrings); i++ {
 		splitLine := strings.Split(gameStrings[i], ": ")
 		id, _ := strconv.Atoi(strings.Split(splitLine[0], " ")[1])
-		sets := strings.Split(splitLine[1], ";")
+		sets := strings.Split(splitLine[1], "; ")
 
 		blueCount, redCount, greenCount := getColourCount(sets)
 
@@ -77,6 +87,7 @@ func getGamesPossible(games map[int][]int) []int {
 	var possibleIds []int
 
 	for id, values := range games {
+		fmt.Printf("Id: %d, Blue: %d, Red: %d, Green %d\n", id, values[0], values[1], values[2])
 		if values[0] <= MaxBlue &&
 			values[1] <= MaxRed &&
 			values[2] <= MaxGreen {
